7db03d59447718c22c4e34a39891cb897e7dfa51
arajo <joarario92@gmail.com>
2021-07-15 23:11:52 +0900

    studying

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,15 +1,60 @@
 import FormView from '../views/FormView.js';
+import ResultView from '../views/ResultView.js';
+import TabView from '../views/TabView.js';
+import KeywordView from '../views/KeywordView.js';
+
+// model
+import SearchModel from '../models/SearchModel.js';
+import KeywordModel from '../models/KeywordModel.js';
 
 const tag = '[MainController]';
 
 export default {
 	init() {
-		console.log(tag, 'init()');
 		FormView.setup(document.querySelector('form'))
 			.on('@submit', e => this.onSubmit(e.detail.input))
+			.on('@reset', e => this.onResetForm())
+
+		TabView.setup(document.querySelector('#tabs'))
+			.on('@change', e => this.onChangeTab(e.detail.tabName))
+
+		KeywordView.setup(document.querySelector('#searchKeyword'))
+		ResultView.setup(document.querySelector('#searchResult'))
+
+		this.selectedTab = '추천 검색어';
+		this.renderView();
+	},
+
+	renderView() {
+		console.log(tag, 'renderView()');
+		TabView.setActiveTab(this.selectedTab);
+
+		if(this.selectedTab === '추천 검색어') {
+			this.fetchSearchKeyword();
+		}else {
+
+		}
+	},
+
+	fetchSearchKeyword() {
+		KeywordModel.list().then(data => {
+			KeywordView.render(data)
+		})
+	},
+
+	search(query) {
+		SearchModel.list(query).then(data => {
+			this.onSearchReset(data)
+		})
 	},
 
 	onSubmit(input) {
 		console.log(tag, 'onSubmit()', input);
+		this.search(input)
+	},
+
+	onResetForm() {
+		console.log(tag, 'onResetForm()');
+		ResultView.hide()
 	}
 }
