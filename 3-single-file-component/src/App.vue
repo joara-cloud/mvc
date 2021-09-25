cc6d2b17cfc7e6a6cd148f9a4abbcc37189ffca4
arajo <joarario92@gmail.com>
2021-09-25 14:07:10 +0900

    단일컴포넌트 적용중

---
+++ 3-single-file-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header>
+      <h2 class="container">검색</h2>
+    </header>
+    <!-- <form-component></form-component> -->
+  </div>
+</template>
+
+<script>
+// import FormComponent from './components/FormComponent.vue.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    // FormComponent
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ 3-single-file-component/src/components/FormComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    sdf
+    </div>
+</template>
+
+<script>
+export default {
+    // props: ['vaule'],
+    data() {
+        return {
+            // inputValue: this.vule
+        }
+    },
+    watch: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
